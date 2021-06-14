01493e003738b1e5d2eb2dc294b66db1d8d170e4
SN0WF14K3 <devsnowflake107@gmail.com>
2021-06-14 22:00:29 +0545

    backup

--- mod.ts
+++ mod.ts
@@ -1,3 +1,5 @@
+// original version: https://github.com/DjDeveloperr/ActivitiesBot
+
 import * as slash from "https://raw.githubusercontent.com/harmonyland/harmony/main/deploy.ts";
 
 // Pick up TOKEN and PUBLIC_KEY from ENV.
@@ -88,7 +90,7 @@ slash.handle("activity", (d) => {
     })
     .then((inv) => {
       d.reply(
-        `[Click here to start ${activity.name} in ${channel.name}.](<https://discord.gg/${inv.code}>)`
+        `[Click here to start **${activity.name}** in **${channel.name}**.](https://discord.gg/${inv.code})`
       );
     })
     .catch((e) => {
@@ -99,9 +101,8 @@ slash.handle("activity", (d) => {
 
 slash.handle("invite", (d) => {
   d.reply(
-    `• [Click here to invite.](<https://discord.com/api/oauth2/authorize?client_id=819835984388030464&permissions=1&scope=applications.commands%20bot>)\n` +
-      `• [Check out Source Code.](<https://github.com/DjDeveloperr/ActivitiesBot>)\n` +
-      `• [Join our Discord.](<https://discord.gg/WVN2JF2FRv>)`,
+    `• [Click here to invite.](<https://discord.com/api/oauth2/authorize?client_id=674970405865390092&permissions=1&scope=applications.commands%20bot>)\n` +
+    `• [Join our Discord.](<https://discord.gg/uqB8kxh>)`,
     { ephemeral: true }
   );
 });
